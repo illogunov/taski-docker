db960df56eed1ddbdc18332a0df787b66913c706
illogunov <il.logunov@gmail.com>
2026-05-02 23:01:07 +0300

    fix github actions ci

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,21 +1,36 @@
-	# backend/api/tests.py
+# backend/api/tests.py
 from http import HTTPStatus
 
-from api import models
 from django.test import Client, TestCase
+from django.urls import reverse
+
+from api import models
+
 
 class TaskiAPITestCase(TestCase):
     def setUp(self):
         self.guest_client = Client()
+        self.tasks_url = reverse('tasks-list') 
 
     def test_list_exists(self):
         """Проверка доступности списка задач."""
-        response = self.guest_client.get('/api/tasks/')
+        response = self.guest_client.get(self.tasks_url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_task_creation(self):
         """Проверка создания задачи."""
-        data = {'title': 'Test', 'description': 'Test'}
-        response = self.guest_client.post('/api/tasks/', data=data)
+        data = {
+            'title': 'Test',
+            'description': 'Test'
+        }
+
+        response = self.guest_client.post(
+            self.tasks_url,
+            data=data,
+            content_type='application/json'
+        )
+
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
-        self.assertTrue(models.Task.objects.filter(title='Test').exists())
+        self.assertTrue(
+            models.Task.objects.filter(title='Test').exists()
+        )
